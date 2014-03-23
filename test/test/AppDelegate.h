87f841852d21f104e5daeabdb948407cd973b26c
Serg <iSerg@Sergs-Mac-Pro.local>
2014-03-23 21:41:41 +0200

    1 commit

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Serg on 3/23/14.
+//  Copyright (c) 2014 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
