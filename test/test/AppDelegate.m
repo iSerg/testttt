9949d238001e29301b4a1f269e52c3a1e9c08833
Serg <iSerg@Sergs-Mac-Pro.local>
2014-03-23 21:58:16 +0200

    commit2

--- test/test/AppDelegate.m
+++ test/test/AppDelegate.m
@@ -8,7 +8,7 @@
 
 #import "AppDelegate.h"
 #import "FirstVC.h"
-
+/////test
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
